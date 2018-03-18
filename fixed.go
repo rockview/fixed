d6bc39eb263e4fa5ef8884288e1829e7612f3273
Jeremy Hall <jeremyahall@gmail.com>
2018-03-17 17:17:28 -0700

    Inital 16.16 version. Not quite working.

--- fixed.go
+++ fixed.go
@@ -2,20 +2,19 @@ package fixed
 
 import(
     "fmt"
-    "math"
 )
 
-type Fixed float64
+type Fixed int32
 
 var min Fixed
 var max Fixed
 
 const(
-    Zero    = Fixed(0.0)
-    One     = Fixed(1.0)
-    Half    = Fixed(0.5)
-    Third   = Fixed(1.0/3.0)
-    Sixth   = Fixed(1.0/6.0)
+    Zero    = Fixed(0x00000000)
+    One     = Fixed(0x00010000)
+    Half    = Fixed(0x00008000)
+    Third   = Fixed(0x00005555)
+    Sixth   = Fixed(0x00002aaa)
 )
 
 func record(f Fixed) {
@@ -27,35 +26,41 @@ func record(f Fixed) {
 }
 
 func FromInt(i int) Fixed {
-    result := Fixed(i)
+    result := Fixed(i<<16)
     record(result)
     return result
 }
 
 func ToInt(x Fixed) int {
     record(x)
-    return int(math.Round(float64(x)))
+    return int((x + Half)>>16)
 }
 
 func FromFloat(f float64) Fixed {
-    result := Fixed(f)
+    result := Fixed(f*65536.0)
     record(result)
     return result
 }
 
 func ToFloat(x Fixed) float64 {
     record(x)
-    return float64(x)
+    return float64(x)/65536.0
 }
 
 func Mul(x, y Fixed) Fixed {
-    result := Fixed(x*y)
+    result := Fixed((int64(x)*int64(y))>>16)
     record(result)
     return result
 }
 
 func Div(x, y Fixed) Fixed {
-    result := Fixed(x/y)
+    result := Fixed((int64(x)<<16)/int64(y))
+    record(result)
+    return result
+}
+
+func Mod(x, y Fixed) Fixed {
+    result := Fixed((int64(x)<<16)%int64(y))
     record(result)
     return result
 }
@@ -70,25 +75,22 @@ func Abs(x Fixed) Fixed {
 }
 
 func Floor(x Fixed) Fixed {
-    result := Fixed(math.Floor(ToFloat(x)))
+    result := x&^Fixed(0xffff)
     record(result)
     return result
 }
 
 func Ceil(x Fixed) Fixed {
-    result := Fixed(math.Ceil(ToFloat(x)))
+    result := x
+    if uint32(x)&0xffff != 0 {
+        result = Floor(x + One)
+    }
     record(result)
     return result
 }
 
 func Round(x Fixed) Fixed {
-    result := Fixed(math.Round(ToFloat(x)))
-    record(result)
-    return result
-}
-
-func Mod(x, y Fixed) Fixed {
-    result := Fixed(math.Mod(ToFloat(x), ToFloat(y)))
+    result := Floor(x + Half)
     record(result)
     return result
 }
