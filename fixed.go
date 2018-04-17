b9fbc54f3cec1664b50968104a06d1dab81ccfa9
Jeremy Hall <jeremyahall@gmail.com>
2018-04-17 12:30:04 -0700

    Add license. Remove range code.

--- fixed.go
+++ fixed.go
@@ -1,13 +1,32 @@
+// MIT License
+//
+// Copyright 2018 Jeremy Hall
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Quick and dirty 16.16 fixed point implementation to test suitability for use
+// with the Simplex Noise Algorithm. No attempt is made to handle overflow or
+// underflow.
+
 package fixed
 
-import(
-    "fmt"
-)
-
 type Fixed int32
-
-var min Fixed
-var max Fixed
 
 const(
     Zero    = Fixed(0x00000000)
@@ -17,85 +36,52 @@ const(
     Sixth   = Fixed(0x00002aaa)
 )
 
-func record(f Fixed) {
-    if f > max {
-        max = f
-    } else if f < min {
-        min = f
-    }
-}
-
 func FromInt(i int) Fixed {
-    result := Fixed(i<<16)
-    record(result)
-    return result
+    return Fixed(i<<16)
 }
 
 func ToInt(x Fixed) int {
-    record(x)
     return int((x + Half)>>16)
 }
 
 func FromFloat(f float64) Fixed {
-    result := Fixed(f*65536.0)
-    record(result)
-    return result
+    return Fixed(f*65536.0)
 }
 
 func ToFloat(x Fixed) float64 {
-    record(x)
     return float64(x)/65536.0
 }
 
 func Mul(x, y Fixed) Fixed {
-    result := Fixed((int64(x)*int64(y))>>16)
-    record(result)
-    return result
+    return Fixed((int64(x)*int64(y))>>16)
 }
 
 func Div(x, y Fixed) Fixed {
-    result := Fixed((int64(x)<<16)/int64(y))
-    record(result)
-    return result
+    return Fixed((int64(x)<<16)/int64(y))
 }
 
 func Mod(x, y Fixed) Fixed {
-    result := Fixed(int64(x)%int64(y))
-    record(result)
-    return result
+    return Fixed(int64(x)%int64(y))
 }
 
 func Abs(x Fixed) Fixed {
-    result := x
     if x < 0 {
-        result = -x
+        return -x
     }
-    record(result)
-    return result
+    return x
 }
 
 func Floor(x Fixed) Fixed {
-    result := x&^Fixed(0xffff)
-    record(result)
-    return result
+    return x&^Fixed(0xffff)
 }
 
 func Ceil(x Fixed) Fixed {
-    result := x
     if uint32(x)&0xffff != 0 {
-        result = Floor(x + One)
+        return Floor(x + One)
     }
-    record(result)
-    return result
+    return x
 }
 
 func Round(x Fixed) Fixed {
-    result := Floor(x + Half)
-    record(result)
-    return result
-}
-
-func ReportLimits() {
-    fmt.Printf("Fixed.min: %f\n", ToFloat(min))
-    fmt.Printf("Fixed.max: %f\n", ToFloat(max))
+    return Floor(x + Half)
 }
